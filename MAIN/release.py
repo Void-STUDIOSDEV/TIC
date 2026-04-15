5587a7f9aea152ba946dacb5e67bd25a4b73fe62
Void-STUDIOSDEV <voiddevr@protonmail.com>
2026-04-15 13:25:00 -0400

    Fix bugs (Detailed here)
    The bugs I have fixed are: Input for ADDITION [+], which made it not work when it gets to variable "b". Made a custom error for division by zero in DIVISION [/]. Fixed a potential computation error in PERCENT SOMETHING IS [>]...... Happy hunting! And enjoy the program! MAY ALL WITH LOW RAM AND TERRIBLE CPU RISE

--- MAIN/release.py
+++ MAIN/release.py
@@ -4,6 +4,7 @@ sleep = time.sleep
 
 
 sleep(0.5); print("Text Interface Calculator, TIC")
+sleep(0.5); print("Version 01.00.01")
 sleep(0.5); print("COPYRIGHT VOID STUDIOS, VOID STUDIOS SOURCE PROGRAM 2025-2026")
 sleep(0.5); print("GNU General Public License 2.x")
 sleep(0.5); print("Reconstruction of Py-Calculator")
@@ -19,55 +20,65 @@ while True:
 		sleep(0.5); print("Goodbye!")
 		break
 		
-	if c == "+":
+	elif c == "+":
 		a = float(input("\nENTER NUMBER: "))
 		b = float(input("ENTER SECOND: "))
-		total = a + B
+		total = a + b
 		print(f"TOTAL: {total}")
 		
-	if c == "X":
+	elif c == "X":
 		a = float(input("\nENTER NUMBER: "))
-		b = float(inout("ENTER SECOND: "))
+		b = float(input("ENTER SECOND: "))
 		total = a * b
 		print(f"TOTAL: {total}")
 		
-	if c == "/":
+	elif c == "/":
 		a = float(input("\nENTER NUMBER: "))
 		b = float(input("ENTER SECOND: "))
 		total = a / b
 		print(f"TOTAL: {total}")
 	
-	if c == "-":
+	elif c == "-":
 		a = float(input("\nENTER NUMBER: "))
-		b - float(input("ENTER SECOND: "))
-		total = a - b
+		b = float(input("ENTER SECOND: "))
+		
+		if b == 0:
+			if a == 0:
+				print("TOTAL: Indeterminate")
+			else:
+				print("TOTAL: Undefined")
+		
+		else:
+			total = a / b
+			print(f"TOTAL: {total}")
+		
 		print(f"TOTAL: {total}")
 		
-	if c == "^":
+	elif c == "^":
 		base = float(input("\nENTER NUMBER: "))
 		exponent = float(input("ENTER SECOND: "))
 		total = base ** exponent
 		print(f"TOTAL: {total}")
 		
-	if c == "*":
+	elif c == "*":
 		a = float(input("\nENTER NUMBER: "))
 		b = float(input("ENTER SECOND: "))
 		total = a % b
 		print(f"TOTAL: {total}")
 		
-	if c == "%":
+	elif c == "%":
 		percent = float(input("\nENTER NUMBER: "))
 		num = float(input("ENTER SECOND: "))
 		total = (percent / 100) * num
 		print(f"TOTAL: {total}")
 		
-	if c == ">":
+	elif c == ">":
 		num = float(input("\nENTER NUMBER: "))
 		total = float(input("ENTER SECOND: "))
-		percent = (num / total) * num
-		print(f"{num} is {percent}& of {total}")
+		percent = (num / total) * 100
+		print(f"{num} is {percent}% of {total}")
 		
-	if c not in ["+", "X", "/", "-", "^", "*", "%", ">"]:
+	elif c not in ["+", "X", "/", "-", "^", "*", "%", ">"]:
 		print("NOT A VALID INPUT.\n")
 		
 		
